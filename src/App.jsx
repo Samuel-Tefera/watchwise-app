925edff98c74487a07786cb24946e79a2f3a1908
Samuel-Tefera <samuelteferab@gmail.com>
2025-07-18 19:29:07 +0300

    build page routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import AppLayout from './layout/AppLayout';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import MovieDetail from './pages/MovieDetail';
+import NotFound from './pages/NotFound';
 
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/discover" element={<Movies />} />
+          <Route path="/movie/:id" element={<MovieDetail />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
