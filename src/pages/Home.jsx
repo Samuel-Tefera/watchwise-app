e95278980b8efc29b59bcd71d7bde1218bb0425f
Samuel-Tefera <samuelteferab@gmail.com>
2025-07-18 19:25:53 +0300

    build app pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Hero from '../layout/Hero';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
---
+++ src/pages/Movies.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MovieGrid from '../features/Movie/MovieGrid';
+import MoviesList from '../features/Movie/MoviesList';
+
+export default function Movies() {
+  return (
+    <div>
+      <MoviesList />
+    </div>
+  );
+}
