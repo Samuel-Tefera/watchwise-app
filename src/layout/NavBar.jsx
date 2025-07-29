7266d61994942f234236b2844d33f5d176fe3490
Samuel-Tefera <samuelteferab@gmail.com>
2025-07-29 21:00:08 +0300

    update app logo

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { FaFilm } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 export default function NavBar() {
   return (
     <nav className="bg-dark border-b border-stone-800 fixed top-0 w-full flex items-center justify-between py-4 px-24 z-50">
-      <div className="text-primary font-semibold text-3xl">🎬 WatchWise</div>
+      <Link
+        to="/"
+        className="text-primary font-semibold text-3xl flex items-center justify-center gap-3"
+      >
+        <FaFilm /> WatchWise
+      </Link>
       <ul className="flex gap-5 items-center justify-between font-semibold text-primary text-xl uppercase">
         <li>
           <Link to="/discover" className="hover:text-primary-hover" href="#">
