cd7897b9ccf0c818427cd567b024d9f07c52e5e1
Samuel-Tefera <samuelteferab@gmail.com>
2025-08-08 14:13:12 +0300

    adjust padding for sm size

--- src/features/Movie/MovieGrid.jsx
+++ src/features/Movie/MovieGrid.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MovieGrid = ({ children }) => {
   return (
-    <div className="py-8 px-16 grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-6">
+    <div className="py-8 px-6 md:px-16 sm:px-8 grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-6">
       {children}
     </div>
   );
