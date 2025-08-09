e0352b1b17e578193ea79c9805dc2a7d9c880bcb
Samuel-Tefera <samuelteferab@gmail.com>
2025-08-09 12:36:08 +0300

    update app layout style

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from './NavBar';
-import Hero from './Hero';
+import { Outlet } from 'react-router-dom';
 
 export default function AppLayout() {
   return (
-    <>
+    <div className="h-screen flex flex-col">
       <NavBar />
-      <Hero />
-    </>
+      <main className="flex-1 overflow-y-auto py-[4.35rem]">
+        <Outlet />
+      </main>
+    </div>
   );
 }
